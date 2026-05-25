b5cce7682b3cbdf9abd964d86dd859b451ea9c19
Rajan Himang <rajanhimang@gmail.com>
2026-05-25 22:44:06 +1000

    Added Search Features

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -47,73 +47,16 @@ const tempWatchedData = [
   },
 ];
 
-const KEY = "e7950c33";
-// http://www.omdbapi.com/?i=tt3896198&apikey=e7950c33
-
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-  const [query, setQuery] = useState("inception");
-
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  const tempQuery = "interstellar";
-
-  // useEffect(function () {
-  //   console.log("After the initial render")
-  // }, [])
-
-  // useEffect(function() {
-  //   console.log("After every render")
-  // })
-
-  // useEffect(function() {
-  //   console.log("D")
-  // }, [query])
-
-  // console.log("During render")
-
-  useEffect(
-    function () {
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-          );
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
-          setMovies(data.Search);
-        } catch (err) {
-          console.error(err.message);
-          setError(err.message);
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (!query.length < 3) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-
-      fetchMovies();
-    },
-    [query],
-  );
-
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
   return (
     <>
       <NavBar>
-        <Search query={query} setQuery={setQuery} />
+        <Search />
         <NumResults movies={movies} />
       </NavBar>
       <Main>
@@ -127,10 +70,7 @@ export default function App() {
           }
         /> */}
         <Box>
-          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
-          {isLoading && <Loader />}
-          {!isLoading && !error && <MovieList movies={movies} />}
-          {error && <ErrorMessage message={error} />}
+          <MovieList movies={movies} />
         </Box>
 
         <Box>
@@ -139,18 +79,6 @@ export default function App() {
         </Box>
       </Main>
     </>
-  );
-}
-
-function Loader() {
-  return <p className="loader">Loading...</p>;
-}
-
-function ErrorMessage({ message }) {
-  return (
-    <p className="error">
-      <span>🛑</span> {message}
-    </p>
   );
 }
 
@@ -172,7 +100,8 @@ function Logo() {
   );
 }
 
-function Search({ query, setQuery }) {
+function Search() {
+  const [query, setQuery] = useState("");
   return (
     <input
       className="search"
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 
-import StarRating from "./StarRating";
-function Test() {
-  const [movieRating, setMovieRating] = useState(0)
+// import StarRating from "./StarRating";
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0)
 
-  return <div>
+//   return <div>
 
-  <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-  <p>This movie was rated {movieRating} stars</p>
-  </div>
-}
+//   <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//   <p>This movie was rated {movieRating} stars</p>
+//   </div>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages={["Terrible", 'Bad', "Okay", 'Good', "Amazing"]} />
+    <App />
+    {/* <StarRating maxRating={5} messages={["Terrible", 'Bad', "Okay", 'Good', "Amazing"]} />
     <StarRating size={24} color="red" className="test" defaultRating={2}/>
-    <Test/>
+    <Test/> */}
   </React.StrictMode>,
 );
